3e2738ab6f6eb8080f875e1f6fabd636d4d4fc43
asadshahab1 <asadshahab02@gmail.com>
2025-06-29 22:56:00 +0500

    Removed splash screen and changed outDir name from build to dist

--- src/index.tsx
+++ src/index.tsx
@@ -6,14 +6,14 @@ import routes from 'src/configs/routesConfig';
 import { worker } from '@mock-utils/mswMockAdapter';
 import { API_BASE_URL } from '@/utils/apiFetch';
 
-async function mockSetup() {
-	return worker.start({
-		onUnhandledRequest: 'bypass',
-		serviceWorker: {
-			url: `${API_BASE_URL}/mockServiceWorker.js`
-		}
-	});
-}
+// async function mockSetup() {
+// 	return worker.start({
+// 		onUnhandledRequest: 'bypass',
+// 		serviceWorker: {
+// 			url: `${API_BASE_URL}/mockServiceWorker.js`
+// 		}
+// 	});
+// }
 
 /**
  * The root element of the application.
@@ -24,20 +24,24 @@ if (!container) {
 	throw new Error('Failed to find the root element');
 }
 
-mockSetup().then(() => {
-	/**
-	 * The root component of the application.
-	 */
-	const root = createRoot(container, {
-		onUncaughtError: (error, errorInfo) => {
-			console.error('UncaughtError error', error, errorInfo.componentStack);
-		},
-		onCaughtError: (error, errorInfo) => {
-			console.error('Caught error', error, errorInfo.componentStack);
-		}
-	});
+// mockSetup().then(() => {
+// 	/**
+// 	 * The root component of the application.
+// 	 */
+// 	const root = createRoot(container, {
+// 		onUncaughtError: (error, errorInfo) => {
+// 			console.error('UncaughtError error', error, errorInfo.componentStack);
+// 		},
+// 		onCaughtError: (error, errorInfo) => {
+// 			console.error('Caught error', error, errorInfo.componentStack);
+// 		}
+// 	});
 
-	const router = createBrowserRouter(routes);
+// 	const router = createBrowserRouter(routes);
 
-	root.render(<RouterProvider router={router} />);
-});
+// 	root.render(<RouterProvider router={router} />);
+// });
+
+const router = createBrowserRouter(routes);
+const root = createRoot(container);
+root.render(<RouterProvider router={router} />);
