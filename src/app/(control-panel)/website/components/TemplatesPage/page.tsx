076a1b7b2d94f24586524d1dfc1dcfdfeb4d347a
jawwadkareem <karimjawwad09@gmail.com>
2025-07-27 11:29:39 +0500

    Template page header changed

--- src/app/(control-panel)/website/components/TemplatesPage/page.tsx
+++ src/app/(control-panel)/website/components/TemplatesPage/page.tsx
@@ -20,7 +20,7 @@ const Templatespage: React.FC = () => {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16 sm:py-20 lg:py-24">
           <div className="text-center">
             <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold mb-4 animate-fade-in">
-              Refund Policy
+              Templates & Mini-APPS
             </h1>
             <p className="text-lg sm:text-xl lg:text-2xl text-teal-100 max-w-4xl mx-auto animate-fade-in">
               Discover Your Full Power – Smart Automation, Precise Optimization,
